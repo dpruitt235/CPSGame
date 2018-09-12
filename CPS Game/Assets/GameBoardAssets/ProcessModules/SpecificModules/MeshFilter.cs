47d2689363e6fa6876422ac551cd134866d451ad
DavidPruitt <husky235@gmail.com>
2018-09-12 18:11:31 -0500

    Added Prefab and Script for MeshFilter, WaterPipeSensor, WaterPump, Water Tank and WaterTankSensor

---
+++ CPS Game/Assets/GameBoardAssets/ProcessModules/SpecificModules/MeshFilter.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshFilter : ProcessModule {
+
+    public int dirt = 0;
+    public int dirtLimit = 10;
+    public bool waterFlow = false;
+
+	// Use this for initialization
+	void Start () {
+        this.displayFields.Add("dirt");
+        this.displayFields.Add("dirtLimit");
+        this.displayFields.Add("waterFlow");
+	}
+}
