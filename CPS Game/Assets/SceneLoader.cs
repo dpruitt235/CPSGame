4ad21fcc34e9c691a0bfdc360675f2bd69f3ae80
Jordan Kubot <jkubs15@gmail.com>
2018-09-12 21:03:46 -0500

    Added menu system for scene traversal

---
+++ CPS Game/Assets/SceneLoader.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour {
+
+	public void LoadNextScene()
+    {
+        int current_scene_index = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(current_scene_index + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void DefenderWins()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void AttackerWins()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
