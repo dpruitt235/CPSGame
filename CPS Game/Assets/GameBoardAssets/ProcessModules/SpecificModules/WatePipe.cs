60ad84397b10e1594483e11a86a33f31f8ede9f0
David Pruitt <dpruitt235@gmail.com>
2018-09-16 11:33:15 -0500

    Added Water Pipe Prefab
    Simple water pipe prefab

---
+++ CPS Game/Assets/GameBoardAssets/ProcessModules/SpecificModules/WatePipe.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatePipe : ProcessModule {
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+}
